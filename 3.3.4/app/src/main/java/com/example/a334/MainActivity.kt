ff8d0577b1e16ec2d65072f24a5dbf41cd5df125
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-18 18:35:05 +0300

    add 3.3.4

---
+++ 3.3.4/app/src/main/java/com/example/a334/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a334
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("TAG", "HelloWorld!")
+    }
+}
