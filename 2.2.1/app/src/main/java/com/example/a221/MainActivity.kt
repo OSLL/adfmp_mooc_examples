774a786472411b2ad77b0ef7be83979004a588ff
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-19 20:52:57 +0300

    add 2.2.1

---
+++ 2.2.1/app/src/main/java/com/example/a221/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.a221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val x = 6
+        when(x) {
+            5->println("==5")
+            6,7->println("6 or 7")
+            in 1..10->println("1..10")
+            in 200..202->{print("5");println("6")}
+            else -> println("nothing")
+        }
+
+        when {
+            x==5-> println("==5")
+            x!=6->println("!=6")
+            x==7 || x==8->println("7,8")
+            else -> println("nothing")
+        }
+    }
+}
