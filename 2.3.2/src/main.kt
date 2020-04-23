ab7f89690eadbae717390b51ba996aaf28a11989
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-23 18:25:59 +0300

    upd 2.3.5

--- 2.3.2/src/main.kt
+++ 2.3.2/src/main.kt
@@ -1,5 +1,3 @@
-package com.example.a232
-
 class Rectangle {
     public var h: Int = 0      // default - public
     private var w: Int = 0
--- 2.3.5/src/main.kt
+++ 2.3.5/src/main.kt
@@ -1,5 +1,3 @@
-package com.example.a235
-
 class Rectangle(h:Int, w:Int) {
     public var h: Int = h
     var w: Int = w
@@ -17,3 +15,10 @@ class Rectangle1(h:Int, w:Int) {
 
 
 class Rectangle2(public var h:Int, var w:Int)
+
+
+fun main() {
+    val r = Rectangle(5, 4)
+    val r1 = Rectangle1(5, 4)
+    val r2 = Rectangle(5, 4)
+}
