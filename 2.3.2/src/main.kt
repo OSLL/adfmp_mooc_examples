7aaec39ae30ce7d6327e112b4e8423d2aafac094
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-23 18:22:54 +0300

    upd 2.3.2

---
+++ 2.3.2/src/main.kt
@@ -0,0 +1,19 @@
+package com.example.a232
+
+class Rectangle {
+    public var h: Int = 0      // default - public
+    private var w: Int = 0
+        get(){return field}        // override
+        set(v){field=v}            // override
+
+    public fun sq(): Int{      // method
+        return h*w
+    }
+
+}
+
+fun main() {
+    val r = Rectangle()
+    println("${r.h}")
+    // u cant println("${r.w}") because w is private
+}
