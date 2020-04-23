35a185ba2b4ae092001964e12038f32ce7637998
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-23 18:32:57 +0300

    upd 2.3.6

--- 2.3.6/src/main.kt
+++ 2.3.6/src/main.kt
@@ -1,10 +1,14 @@
-package com.example.a236
-
 class Rectangle(var h: Int, var w: Int) {
     constructor(x1:Int, y1:Int, x2:Int, y2:Int):
             this(y2-y1, x2-x1)
     {
         //code
-        print("!")
+        print("it's constructor's code!")
     }
+}
+
+
+fun main() {
+    val r = Rectangle(5, 6)
+    val rx = Rectangle(0, 0, 5, 6)
 }
